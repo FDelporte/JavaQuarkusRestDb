777d0615c1d9781614483f006c3735848d643ed0
Frank Delporte <f.delporte@televic.com>
2020-07-18 23:45:02 +0200

    Initial commit

---
+++ src/test/java/be/webtechie/javaquarkusrestdb/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package be.webtechie.javaquarkusrestdb;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
